d7bd8aafc43712359329d1a6fb0fb2e73324d55d
ProfRCI <ritaidem@prof.educacao.sp.gov.br>
2025-08-17 14:49:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContraste = document.getElementById(‘alterna-contraste’)
 
     let tamanhoAtualFonte = 1;
 
@@ -16,6 +17,14 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
-
+    const botaoDeAcessibilidade = document.getElementById(‘botaoacessibilidade’);    
+    const opcoesDeAcessibilidade = document.getElementById(‘opcoesacessibilidade’);
+     botaoDeAcessibilidade.addEventListener(‘click’, function (){ 
+         botaoDeAcessibilidade.classList.toggle(‘rotacao-botao’); 
+         opcoesDeAcessibilidade.classList.toggle(‘apresenta-lista’);   
+     })
+    alternaContraste.addEventListener(‘click’, function(){
+        document.body.classList.toggle(‘alto-contraste’) 
+    })
 })
 
